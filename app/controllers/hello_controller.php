c6733f7dec6701fa4140ed1ebac75b7eef392501
asunato <asunato@gmail.com>
2012-06-30 16:01:50 +0900

    add comment

--- app/controllers/hello_controller.php
+++ app/controllers/hello_controller.php
@@ -3,6 +3,7 @@ class HelloController extends AppController
 {
     public function index()
     {
+	// hello world
         $message = Hello::getMessage();
         $this->set(get_defined_vars());
     }
